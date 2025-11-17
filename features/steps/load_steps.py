9c6ca92835e61e8251b149872f0318db7bb96ffa
= <Ryan_Smoot@att.net>
2025-11-16 21:30:16 -0500

    Completed the project

--- features/steps/load_steps.py
+++ features/steps/load_steps.py
@@ -50,9 +50,9 @@ def step_impl(context):
         payload = {
             "name" : row['name'],
             "description" : row['description'],
-            "price" : row['price']
-            "available" : row['available']
+            "price" : row['price'],
+            "available" : row['available'] in ['True', 'true', '1'],
             "category" : row['category']
         }
         context.resp = requests.post(rest_endpoint, json=payload)
-        assert context.resp.status_code == status.HTTP_201_CREATED
+        assert context.resp.status_code == HTTP_201_CREATED
